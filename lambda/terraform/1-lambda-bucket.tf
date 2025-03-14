8ba67a0c706066426a6bd1f74bbc84d48322909a
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2025-03-14 12:49:37 +0000

    added 3 projects

---
+++ lambda/terraform/1-lambda-bucket.tf
@@ -0,0 +1,23 @@
+//in terraform project we typically use zip archive.
+//we build the function with all dependenciesand package it as a zip archive and 
+//upload it in a s3 bucket
+//then when we create lambda, we point to this zip archive object in the s3 bucket
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "lambda"
+  length = 2
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket        = random_pet.lambda_bucket_name.id
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "lambda_bucket" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ lambda/terraform/2-hello-lambda.tf
@@ -0,0 +1,64 @@
+resource "aws_iam_role" "hello_lambda_exec" {
+  name = "hello-lambda"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "hello_lambda_policy" {
+  role       = aws_iam_role.hello_lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "hello" {
+  function_name = "hello"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_hello.key
+
+  runtime = "nodejs16.x"
+  handler = "function.handler"
+  //we need below line to redeploy the function if we change anything in the source code.architectures.architectures
+  //ex: we change sth in code. if the hash of the zip archive is different,it will force to redeploy lambda 
+  source_code_hash = data.archive_file.lambda_hello.output_base64sha256
+
+  role = aws_iam_role.hello_lambda_exec.arn
+}
+
+
+resource "aws_cloudwatch_log_group" "hello" {
+  name = "/aws/lambda/${aws_lambda_function.hello.function_name}"
+
+  retention_in_days = 14
+}
+
+
+data "archive_file" "lambda_hello" {
+  type = "zip"
+
+  source_dir  = "../${path.module}/hello"
+  output_path = "../${path.module}/hello.zip"
+}
+
+
+resource "aws_s3_object" "lambda_hello" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "hello.zip"
+  source = data.archive_file.lambda_hello.output_path
+  //below line calculates the MD5 checksum of the Lambda ZIP file.
+  //If the ZIP file changes, Terraform detects the change via the MD5 hash and re-uploads the file to S3.
+  etag = filemd5(data.archive_file.lambda_hello.output_path)
+}
